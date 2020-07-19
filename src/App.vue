c1eb799f5de1ff7ef8c3ecfb53df25dc7ef4bb4d
liyuxuan <44829092+xmliszt@users.noreply.github.com>
2020-07-19 17:52:09 +0800

    [Feature] Change customized cursor

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,24 @@
 </template>
 
 <style>
+body {
+  cursor: url(https://cur.cursors-4u.net/others/oth-5/oth510.ani),
+    url(http://cur.cursors-4u.net/others/oth-5/oth510.png), default !important;
+}
+
 #app {
   font-family: "Gill Sans", "Gill Sans MT", Calibri, "Trebuchet MS", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.el-button,
+.el-button:hover,
+.el-button:focus {
+  cursor: url(https://cur.cursors-4u.net/others/oth-5/oth510.ani),
+    url(http://cur.cursors-4u.net/others/oth-5/oth510.png), default !important;
 }
 
 #nav {
